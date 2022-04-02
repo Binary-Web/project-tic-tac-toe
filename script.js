432f849c316d3d4084c305ea8223cb003de77a9e
ben <ben.carlo.cornico@gmail.com>
2022-04-01 18:32:41 -0700

    scripts

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-const btnCell = document.querySelectorAll('.btn-cell');
+const board = (() => {
+    let winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-btnCell.forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        console.log(button.childNodes)
-    });
+
+
+    let X = [];
+    let O = [];
+
+    const xTurn = (cellNum) => {
+
+    }
+    const oTurn = (cellNum) => {
+
+    }
+
+    const gameStatus = () => {
+        winningConditions.forEach(condition => {
+            const arr = new Set(condition);
+            if(arr.length == 1) {
+                //we got a winner
+            }
+        })
+    }
+})();
+
+
+const handleClick = (e) => {
+    // click event
+    console.log(e.target.dataset.cellNum);
+}
+
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    cell.addEventListener('click', handleClick)
 })
