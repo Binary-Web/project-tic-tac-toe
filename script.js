7e7f1910889ee937bb5c75ac39727c915fa34214
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-04-01 23:41:41 -0700

    game logic in javascript

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
 const board = document.querySelector('.board');
 var boardWidth = board.offsetWidth;
-board.style.height = `${boardWidth}px`
+board.style.height = `${boardWidth}px`;
 
-
-const gameBoard = (() => {
+const GameBoard = (() => {
+    const player1 = '<span class="mdi mdi-close"></span>';
+    const player2 = '<span class="mdi mdi-checkbox-blank-circle-outline"></span>';
+    let turns = 0;
     let winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -15,35 +18,80 @@ const gameBoard = (() => {
         [2, 4, 6]
     ];
 
-
-
     let X = [];
     let O = [];
 
-    const xTurn = (cellNum) => {
-
+    const p1 = (cellNum, cellBlock) => {
+        // <span class="mdi mdi-close"></span>
+        cellBlock.innerHTML = player1;
+        gameMarker(cellNum, "x");
     }
-    const oTurn = (cellNum) => {
-
+    const p2 = (cellNum, cellBlock) => {
+        // <span class="mdi mdi-checkbox-blank-circle-outline"></span> 
+        cellBlock.innerHTML = player2
+        gameMarker(cellNum, "o");
     }
 
     const gameStatus = () => {
         winningConditions.forEach(condition => {
-            const arr = new Set(condition);
+            const arr = [...new Set(condition)];
             if(arr.length == 1) {
-                //we got a winner
+                declareWinner(arr[0]);
             }
+            console.log(arr)
         })
+        console.log(winningConditions)
+        
+    }
+
+    const gameMarker = (cellNum, symbol) => {
+        winningConditions.forEach(conditions => {
+            conditions.forEach((cellNumber, index) => {
+                if(cellNumber == cellNum) {
+                    conditions[index] = symbol
+                }
+            })
+        });
+        gameStatus();
+    }
+
+    const gameDraw = () => {
+        //NO WINNERS
+    }
+    const playerMoves = (e) => {
+        cellNum = e.target.dataset.cellNum;
+        cellBlock = e.target;
+        if(turns % 2 == 0) {
+            p1(cellNum, cellBlock);
+        } else if(turns % 2 == 1) {
+            p2(cellNum, cellBlock);
+        }
+        console.log(turns)
+        turns++;
+    }
+
+    const declareWinner = (winner) => {
+        alert(`Winner is ${winner}`);
+    }
+    return {
+        playerMoves
     }
 })();
 
 
-const handleClick = (e) => {
-    // click event
-    console.log(e.target.dataset.cellNum);
+class Player {
+    constructor(uname, playerTurn) {
+        this.uname = uname;
+        this.playerTurn = playerTurn
+    }
 }
 
+;
 const cells = document.querySelectorAll('.cell');
+
 cells.forEach(cell => {
-    cell.addEventListener('click', handleClick)
+    cell.style.height = `${boardWidth / 3}px`;
+    cell.style.width = `${boardWidth / 3}px`;
+    cell.addEventListener('click', GameBoard.playerMoves)
 })
+
